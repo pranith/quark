ee917bddd82a11c0955cb919349d76753f3ee476
Pranith Kumar <bobby.prani@gmail.com>
2021-07-14 15:35:22 -0700

    Add RISCV VM setup

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A QEMU based framework for generating instruction traces on x86, ARM, and
 RISCV architectures.
 
 See INSTALL for setup instructions.
+
+See run_usermode.sh for collecting traces from binaries using qemu-user.
---
+++ run_usermode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Usage: ./run.sh <architecture> <benchmark>
+# Supported architectures are x86_64, aarch64, riscv64
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: ./run.sh <architecture> <benchmark>"
+	exit 1
+fi
+
+arch=$1
+benchmark=$2
+bench_name="$(basename $benchmark)"
+./qemu/relbuild/qemu-${arch} -d plugin -plugin ./qpoints/libbbv.so,arg=${bench_name} ${benchmark}
+
+./simpoints/bin/simpoint -inputVectorsGzipped -loadFVFile ${bench_name}_bbv.gz -k 10 -saveSimpoints ${bench_name}.simpts -saveSimpointWeights ${bench_name}.weights
+
+./qemu/relbuild/qemu-${arch} -d plugin -plugin ./qpoints/libtracer.so,arg=${bench_name},arg=${arch} ${benchmark}
+
