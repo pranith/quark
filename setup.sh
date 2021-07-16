c0ae6a848a484027ed2c9ef599341edc09ddda2d
Pranith Kumar <bobby.prani@gmail.com>
2021-07-15 19:24:47 -0700

    Fix capstone dependency and ignore uboot/opensbi for now

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ git submodule update --init --recursive
 pushd qemu
 mkdir relbuild
 pushd relbuild
-../configure --target-list=aarch64-softmmu,x86_64-softmmu,riscv64-softmmu,aarch64-linux-user,x86_64-linux-user,riscv64-linux-user --enable-plugins
+../configure --target-list=aarch64-softmmu,x86_64-softmmu,riscv64-softmmu,aarch64-linux-user,x86_64-linux-user,riscv64-linux-user --enable-plugins --enable-capstone=git
 make -j4
 popd
 popd
@@ -16,18 +16,18 @@ pushd simpoints
 make
 popd
 
-pushd riscv/u-boot
-CROSS_COMPILE=riscv64-linux-gnu- make qemu-riscv64_smode_defconfig
-CROSS_COMPILE=riscv64-linux-gnu- make -j4
-popd
+#pushd riscv/u-boot
+#CROSS_COMPILE=riscv64-linux-gnu- make qemu-riscv64_smode_defconfig
+#CROSS_COMPILE=riscv64-linux-gnu- make -j4
+#popd
 
-pushd riscv/opensbi
-CROSS_COMPILE=riscv64-linux-gnu- make PLATFORM=generic FW_PAYLOAD_PATH=../u-boot/u-boot.bin
-popd
+#pushd riscv/opensbi
+#CROSS_COMPILE=riscv64-linux-gnu- make PLATFORM=generic FW_PAYLOAD_PATH=../u-boot/u-boot.bin
+#popd
 
-mkdir riscv/image
-pushd riscv/image
-wget https://gitlab.com/api/v4/projects/giomasce%2Fdqib/jobs/artifacts/master/download?job=convert_riscv64-virt -O artifacts.zip
-unzip artifacts.zip
-popd
+#mkdir riscv/image
+#pushd riscv/image
+#wget https://gitlab.com/api/v4/projects/giomasce%2Fdqib/jobs/artifacts/master/download?job=convert_riscv64-virt -O artifacts.zip
+#unzip artifacts.zip
+#popd
 
